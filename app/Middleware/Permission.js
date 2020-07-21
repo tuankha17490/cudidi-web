3a49d8ebe1fcb743acb2a2de8d7853031dea3d8d
tuankha17490 <tuankha1749@gmail.com>
2020-07-22 00:11:31 +0700

    swagger

--- app/Middleware/Permission.js
+++ app/Middleware/Permission.js
@@ -1,37 +1,71 @@
 import RoleRespository from "../API/modules/roles/respository"
 var module
-var method
-export default class Permissions {
-    static _Instance
-    static Instance(methodName, moduleName) {
-        if (!this._Instance) {
-            this._Instance = new this(methodName, moduleName);
+const Excute = async (req, res, method) => {
+    const roleName = req.userData.Role;
+    const roleFetched = await RoleRespository.Instance().listBy(['*'], {
+            Name: roleName
+        })
+        .withGraphFetched('permissions.[modules, methods]')
+    const data = roleFetched[0].permissions
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].modules[0].Name == module && data[i].methods[0].Name == method) {
+            break
         }
-        return this._Instance;
+        if (i == (data.length - 1)) {
+            return res.status(200).json({
+                status: 403,
+                message: 'error.isNotPermittedToAccess'
+            })
+        }
     }
-    constructor(methodName, moduleName) {
-        method = methodName
+    return true
+}
+export default class Permissions {
+    constructor(moduleName) {
         module = moduleName
     }
 
-    async Excute(req,res,next) {
-        const roleName = req.userData.Role;
-        const roleFetched = await RoleRespository.Instance().listBy(['*'],{Name: roleName})
-        .withGraphFetched('permissions.[modules, methods]')
-       const data = roleFetched[0].permissions
-        for(let i = 0; i < data.length; i++) {
-            if(data[i].modules[0].Name == module && data[i].methods[0].Name == method) {
-                console.log(data[i].modules[0].Name);
-                break
-            }
-            if(i == data.length -1) {
-                return res.status(200).json({
-                    status:403,
-                    message: 'error.isNotPermittedToAccess'
-                })
-            }
-        }
+    async GetList(req, res, next) {
+        const excute = await Excute(req, res, 'GetList')
+        if(excute != true) return excute
         next()
     }
-    
+
+    async Create(req, res, next) {
+        const excute = await Excute(req, res, 'Create')
+        if(excute != true) return excute
+        next()
+    }
+
+    async Read(req, res, next) {
+        const excute = await Excute(req, res, 'Read')
+        if(excute != true) return excute
+        next()
+    }
+
+    async Delete(req, res, next) {
+        const excute = await Excute(req, res, 'Delete')
+        if(excute != true) return excute
+        next()
+    }
+    async Update(req, res, next) {
+        const excute = await Excute(req, res, 'Update')
+        if(excute != true) return excute
+        next()
+    }
+    async Search(req, res, next) {
+        const excute = await Excute(req, res, 'Search')
+        if(excute != true) return excute
+        next()
+    }
+    async JoinClass(req, res, next) {
+        const excute = await Excute(req, res, 'JoinClass')
+        if(excute != true) return excute
+        next()
+    }
+    async UpdateMyUser(req, res, next) {
+        const excute = await Excute(req, res, 'UpdateMyUser')
+        if(excute != true) return excute
+        next()
+    }
 }
